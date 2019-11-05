192980374250d18cce1050667405c829ddd986b3
Stefan <Rosas761@gmail.com>
2019-11-04 19:32:07 -0500

    commit

--- 13-javascript-challenge/StarterCode/static/js/app.js
+++ 13-javascript-challenge/StarterCode/static/js/app.js
@@ -2,3 +2,25 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+
+var tbody = d3.select("tbody");
+var button = d3.select("#filter-btn");
+
+
+data.forEach(Alien =>{
+    var row = tbody.append("tr");
+    Object.entries(Alien).forEach(([key,value]) => {
+    row.append('td').text(value);
+    });
+});
+
+button.on("click", function() {
+    var inputElement = d3.select("#datetime");
+    var inputValue = inputElement.property("value");
+    var row = tbody.append("tr");
+    var filteredData = tableData.filter(stefan => stefan.datetime === inputValue);
+    //var dates = filteredData.map(stefan => stefan.datetime);
+    tbody.html("");
+    tbody.append("td").text(filteredData);
+    console.log(filteredData);
+    });
